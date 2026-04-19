1384f7e8f87572dd089c57a924a88f6fcc847ffd
Deep Detroja <deepdetroja1223@gmail.com>
2026-04-19 22:14:02 +0530

    Model & Schema added

---
+++ Server/src/models/userdb.ts
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    email:{type:String,required:true,unique:true},
+    username:{type:String,required:true},
+    password:{type:String,required:true} //hased password
+},{timestamps:true}); //track createdAt,updatedAt
+
+const  tagSchema=new mongoose.Schema({
+    title:{type:String,requirea:true,unique:true}
+},{timestamps:true});
+
+const conteneSchema=new mongoose.Schema({
+    title:{type:String,requires:true},
+    link:{type:String},
+    type:{type:String},
+    description:{type:String,default:""},
+    tags:[{type:mongoose.Types.ObjectId,ref:'Tag'}],//Many to Many
+    userId:{type:mongoose.Types.ObjectId,ref:'User',required:true}//Many contents to one user
+},{timestamps:true});
+
+const linkSchema=new mongoose.Schema({
+    hash:{type:String,required:true},
+    userId:{type:mongoose.Types.ObjectId,ref:'User',required:true}//1 link per user
+},{timestamps:true});
+
+export const Tag=mongoose.model('Tag',tagSchema);
+export const userModel = mongoose.model('User',userSchema);
+export const contentModel=mongoose.model('Content',conteneSchema);
+export const linkModel = mongoose.model('Link',linkSchema);
